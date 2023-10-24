acea95e48c8c0f299823baae140dec02290f8a60
yash kumar mishra <mishrayash3778@gmail.com>
2023-10-24 21:51:22 +0530

    j

--- src/Components/Contests.jsx
+++ src/Components/Contests.jsx
@@ -22,61 +22,51 @@ const Contests = props => {
 
     return(
         <div className="m-5 mb-5" >
-             <div className="row row-cols-1 row-cols-md-3 mx-3 g-4">
+             <div className="grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 mt-4  gap-16 mx-auto" role="group">
            {
-            contest.map(contest => (
-                <div className="col">
-                    <div className="card bg-gradient-to-r from-indigo-900 via-purple-900 to-pink-900  text-black ">
-                        <div className="card-body">
-                            <ul className="list-group list-group-flush  border border-light border  border-opacity-10">
-                                <li className="list-group-item bg-gradient-to-r from-indigo-900 via-purple-900 to-pink-900  border border-light border  border-opacity-10">
-                                    <h6 className="text-light">
-                                        {
-                                        contest.name
-                                    }</h6>
-                                </li>
-                                <li className="list-group-item bg-gradient-to-r from-indigo-900 via-purple-900 to-pink-900  border border-light border  border-opacity-10">
-                                    <h6 className="text-light">
-                                        {
-                                        search
-                                    }</h6>
-                                </li>
-                                <li className="list-group-item bg-gradient-to-r from-indigo-900 via-purple-900 to-pink-900  border border-light border  border-opacity-10">
-                                    <h6 className="text-light">Status : {
-                                        contest.status
-                                    }</h6>
-                                </li>
-                                <li className="list-group-item bg-gradient-to-r from-indigo-900 via-purple-900 to-pink-900  border border-light border  border-opacity-10">
-                                    <h6 className="text-light">In 24 hour : {
+            contest.map(contest => (      
+    <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 ">
+    <h5 class="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">{search}</h5>
+    <div class="flex items-baseline text-gray-900 dark:text-white">
+        <span class="text-xl font-semibold">{contest.name}</span>
+    </div>
+    <ul role="list" class="space-y-5 my-7">
+        <li class="flex space-x-3 items-center">
+            <svg class="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+            </svg>
+            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">{contest.status}</span>
+        </li>
+        <li class="flex space-x-3">
+            <svg class="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+            </svg>
+            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">In 24 hour : {
                                         contest.in_24_hours
-                                    }</h6>
-                                </li>
-                                <li className="list-group-item bg-gradient-to-r from-indigo-900 via-purple-900 to-pink-900  border border-light border  border-opacity-10">
-                                    <h6 className="text-light">Start Time : {
+                                    }</span>
+        </li>
+        <li class="flex space-x-3">
+            <svg class="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+            </svg>
+            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">Start Time : {
                                         contest.start_time
-                                    }</h6>
-                                </li>
-                                <li className="list-group-item bg-gradient-to-r from-indigo-900 via-purple-900 to-pink-900  border border-light border  border-opacity-10">
-                                    <h6 className="text-light">End Time : {
+                                    }</span>
+        </li>
+        <li class="flex space-x-3">
+            <svg class="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+            </svg>
+            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">End Time : {
                                         contest.end_time
-                                    }</h6>
-                                </li>
-                                <div className="border border-light border  border-opacity-25 ">
-                                <a href={
-                                contest.url
-                            }
-                        
-                            className="btn  text-light bg-gradient-to-r from-indigo-900 via-purple-900 to-pink-900  w-100"
-                            target="_blank"
-                            rel="noreferrer noopener">Register &#10148;</a> 
-                            </div> 
-                            <div className="border border-light border  border-opacity-25">
-                            <button  className="btn  text-light bg-gradient-to-r from-indigo-900 via-purple-900 to-pink-900 w-100"> Add to fav &#9829;</button>
-                    </div>
-                    </ul>
-                        </div>
-                    </div>
-                </div>
+                                    }</span>
+        </li>
+    </ul>
+    <a href={contest.url} target="_blank"
+                             rel="noreferrer noopener">
+    <button type="button" class="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">Register &#10148;</button>
+    </a>
+</div>    
             ))
         }
         </div>
