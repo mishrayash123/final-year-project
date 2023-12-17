3ed81156bacfc4076c7afaf545e7c32dcda0ed2d
yash kumar mishra <mishrayash3778@gmail.com>
2023-12-17 13:50:09 +0530

    kdjks

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import './App.css';
-import Navbar from './Components/Navbar';
-import { Route, Routes, BrowserRouter  } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './Components/Home';
-import Footer from './Components/Footer';
-import Contests from './Components/Contests';
-import Plateforms from './Components/Plateforms';
-import Login from './Components/Login';
-import Register from './Components/Register';
-import Profile from './Components/Profile';
+import "./App.css"
 
 function App() {
   return (
-    <div className='bg-black'>
-      <BrowserRouter>
-      <Navbar />
-      <Routes> 
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/contest' element={<Contests/>}></Route>
-      <Route path='/plateforms' element={<Plateforms/>}></Route>
-      <Route path='/login' element={<Login/>}></Route>
-      <Route path='/register' element={<Register/>}></Route>
-      <Route path='/profile' element={<Profile/>}></Route>
-      </Routes>
-      <Footer/>
-      </BrowserRouter>
+    <div>
+      <p className="text-black font-bold text-2xl text-center m-28">
+      Helo , The Project name is Eng-Track
+    </p>
     </div>
   );
 }
