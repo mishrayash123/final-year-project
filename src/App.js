0f43fad8a63588a20cd4b0338a0d9fa1e0266b51
Vivek Kumar <code.vivekkumar@gmail.com>
2023-12-21 11:03:15 +0530

    navbar-updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import "./App.css"
-// import Singin from "./components/Singin";
-import Navbar from "./components/Navbar";
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+const Home = () => <div>Home Page</div>;
 
 function App() {
   return (
-    <div>
- 
-      <Navbar/>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact component={Home} />
+        {/* Add more routes for other pages */}
+      </Routes>
+    </Router>
   );
 }
 
